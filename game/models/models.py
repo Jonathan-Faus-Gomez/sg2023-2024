e37bedbf17aa3d9eb2adee1837a8cac8d0f05c16
Jonathan-Faus-Gomez <jonfaugom@alu.edu.gva.es>
2023-10-05 15:42:51 +0000

    dino y edificio creadas

--- game/models/models.py
+++ game/models/models.py
@@ -5,8 +5,24 @@ from odoo import models, fields, api
 
 class player(models.Model):
     _name = 'game.player'
-    _description = 'El jugador'
+    _description = 'Jugador'
 
-    name = fields.Char()
+    name = fields.Char(required=True)
     level = fields.Integer()
+    dino = fields.One2many('game.dino','player')
 
+class dino(models.Model):
+    _name= 'game_dino'
+    _description= 'Dinosaurio'
+
+    level = fields.Integer()
+    tipo = fields.Selection([('1', 'Carnivoro'), ('2', 'Herbivoro'), ('3', 'Omnivoro')])
+    player=fields.Many2one('game.player')
+
+
+
+class edificio(models.Model):
+    _name= 'game_edificio'
+    _description= 'Edificio'
+
+    tipo = fields.Selection([('1', 'Almacen'), ('2', 'Defensa'),('3', 'Ataque'), ('4', 'Produccion')])
